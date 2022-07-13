f30c60ab76b4a08b471b41db3a2c3cbe5bf36332
Devansh0901 <B20094@students.iitmandi.ac.in>
2022-07-14 00:49:37 +0530

    add ons

--- LoginUser/cred/urls.py
+++ LoginUser/cred/urls.py
@@ -5,5 +5,9 @@ from cred import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('login',views.loginUser,name='login'),
-    path('logout',views.logoutUser,name='logout')
+    path('logout',views.logoutUser,name='logout'),
+    path('Basetemplate',views.template,name='template'),
+    path('sec1',views.sec1,name='sec1'),
+    path('sec2',views.sec2,name='sec2'),
+    path('sec3',views.sec3,name='sec3')
 ]
